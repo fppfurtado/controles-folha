608c07fe730be1852db5d3c78e8ada45d65342c5
Fernando Furtado <fppfurtado@gmail.com>
2020-04-14 13:59:18 -0300

    remoção de espaços em branco e quebras de linha

--- src/main/webapp/resources/validacaoFrontEnd.js
+++ src/main/webapp/resources/validacaoFrontEnd.js
@@ -10,15 +10,11 @@ function formatarCpf(event) {
 	cpf = cpf.split("");
 
 	if (cpf.length > 3 && cpf[3] != ".") {
-
 		cpf.splice(3, 0, ".");
-
 	}
 
 	if (cpf.length > 7 && cpf[7] != ".") {
-
 		cpf.splice(7, 0, ".");
-
 	}
 
 	if (cpf.length > 11 && cpf[11] != "-") {
